a722bf4b5be340df629e193b71a8a34fd441fd94
Zara <zara@gene.co.uk>
2020-05-08 16:14:18 +0100

    create button, add delete button on note

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Button(props){
+
+  return (
+    <button type="button" className={props.classes} onClick={props.handleClick}>
+      {props.label}
+    </button>
+  );
+  
+}
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,14 @@
 import React from "react";
+import {Button} from './Button';
+
+function deleteNote() {
+  console.log('delete me');
+}
 
 const Note = ({ source }) => (
   <li className="take-note__saved-list-item">  
     <p>{`${source}`}</p>
+    <Button classes="take-note__button" label="Delete" handleClick={deleteNote}/>
   </li>
 );
 
