9164af9f14f0d1d8cbcd722952d663a21968f156
Zara <zara@gene.co.uk>
2020-05-08 12:00:54 +0100

    form submit saves note in localstorage

--- src/ViewNotes.js
+++ src/ViewNotes.js
@@ -2,31 +2,11 @@ import React, {useState} from 'react';
 
 class ViewNotes extends React.Component {
 
-  componentDidMount() {
-  
-    var form = document.querySelector('.js-note-form');
-    form.addEventListener('submit', function(e){
-      var note = document.querySelector('.js-note'),
-          storedNotes = localStorage.getItem("notes"),
-          savedNotes = storedNotes ? JSON.parse(storedNotes) : [];
-
-      // Ignore submit form
-      e.preventDefault();
-
-      // Add our note to the array and empty the textarea
-      savedNotes.push(note.value);
-      note.value = '';
-
-      // Add the array to local storage
-      localStorage.setItem('notes', JSON.stringify(savedNotes));
-    });
-  }
-
   render() {
     return (
       <section className="take-note__section js-saved-notes">
         <h3>Your previously saved notes</h3>
-        <p class="js-saved-notes--empty">Nothing saved yet.</p>
+        <p className="js-saved-notes--empty">Nothing saved yet.</p>
       </section>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,37 @@
 import React from 'react';
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { note: '' };
+  }
+
+  saveNote = (event) => {
+    event.preventDefault();
+    const storedNotes = localStorage.getItem("notes"),
+          savedNotes = storedNotes ? JSON.parse(storedNotes) : [];
+
+    savedNotes.push(this.state.note);
+    this.setState({note: ''});
+    localStorage.setItem('notes', JSON.stringify(savedNotes));
+    this.emptyForm();
+  }
+
+  emptyForm() {
+    const note = document.querySelector('.js-note');
+    note.value = '';
+  }
+
+  updateNoteState = (event) => {
+    this.setState({note: event.target.value});
+  }
 
   render() {
     return (
-      <form className="js-note-form">
+      <form className="js-note-form" onSubmit={this.saveNote}>
           <label htmlFor="note" className="visually-hidden">Write a note that you want to save</label>
-          <textarea className="take-note__input js-note" id="note" required></textarea>
-          <button type="submit" className="take-note__button take-note__button--save js-save-note">Save your note</button>
+          <textarea className="take-note__input js-note" id="note" onChange={this.updateNoteState} required></textarea>
+          <button type="submit" className="take-note__button take-note__button--save">Save your note</button>
       </form>
     );
   }
