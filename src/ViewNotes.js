491c486715ca9db416abddf164f4c93f0174a05e
Zara <zara@gene.co.uk>
2020-05-08 11:33:59 +0100

    notes being saved in local storage but not best way

--- src/ViewNotes.js
+++ src/ViewNotes.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 class ViewNotes extends React.Component {
 
+  componentDidMount() {
+  
+    var form = document.querySelector('.js-note-form');
+    form.addEventListener('submit', function(e){
+      var note = document.querySelector('.js-note'),
+          storedNotes = localStorage.getItem("notes"),
+          savedNotes = storedNotes ? JSON.parse(storedNotes) : [];
+
+      // Ignore submit form
+      e.preventDefault();
+
+      // Add our note to the array and empty the textarea
+      savedNotes.push(note.value);
+      note.value = '';
+
+      // Add the array to local storage
+      localStorage.setItem('notes', JSON.stringify(savedNotes));
+    });
+  }
+
   render() {
     return (
-      <section className="take-note__section">
+      <section className="take-note__section js-saved-notes">
         <h3>Your previously saved notes</h3>
-        <p className="js-saved-notes--empty">Nothing saved yet.</p>
+        <p class="js-saved-notes--empty">Nothing saved yet.</p>
       </section>
     );
   }
